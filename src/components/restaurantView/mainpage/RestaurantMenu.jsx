3d5b0b0a02bc2b2cbec129510a4df668fbc32cc2
Srijan2055 <srijanpandey73@gmail.com>
2022-07-02 12:12:08 +0545

    first commit

---
+++ src/components/restaurantView/mainpage/RestaurantMenu.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Grid
+} from '@material-ui/core';
+import DishCard from "../../card/DishCard";
+import EmptyDish from '../../card/EmptyDish';
+const axios = require('axios').default;
+
+class RestaurantMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu : undefined
+    }
+    this.getAllDishes = this.getAllDishes.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllDishes();
+    
+  }
+
+  getAllDishes() {
+    console.log("running")
+    axios.get("/api/restaurant/menu/" + this.props.currentUser.id).then(
+      response => {
+        this.setState({menu: response.data});
+        console.log(response.data, "resp")
+      }
+    ).catch(err => console.log(err));
+  }
+
+  render() {
+    return this.props.currentUser ? (
+      <Grid container spacing={3} justify="space-evenly">
+        {/* {console.log(typeof this.state.menu, "menu latest")} */}
+        {this.state.menu ?  
+          (this.state.menu.map((dish, index) => (
+            <Grid item xs={3} key={index}>
+              <DishCard dish={dish} getAllDishes={this.getAllDishes} currentUser={this.props.currentUser} />
+
+            </Grid>
+          ))) : null
+        }
+        <Grid item xs={3}>
+          <EmptyDish getAllDishes={this.getAllDishes} currentUser={this.props.currentUser} />
+        </Grid>
+      </Grid>
+    ) : <div>The restaurant is not available</div>
+  }
+}
+
+export default RestaurantMenu;
